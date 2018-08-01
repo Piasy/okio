178cced19e5669fb7b68dd45ef18f5510fad2bd3
Piasy Xu <xz4215@gmail.com>
2018-08-01 20:31:59 +0800

    commend out java.nio.file code

--- okio/src/main/java/okio/Okio.java
+++ okio/src/main/java/okio/Okio.java
@@ -25,13 +25,9 @@ import java.io.InterruptedIOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
-import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
 
 import static okio.Util.checkOffsetAndCount;
 
@@ -168,12 +164,12 @@ public final class Okio {
     return source(new FileInputStream(file));
   }
 
-  /** Returns a source that reads from {@code path}. */
-  @IgnoreJRERequirement // Should only be invoked on Java 7+.
-  public static Source source(Path path, OpenOption... options) throws IOException {
-    if (path == null) throw new IllegalArgumentException("path == null");
-    return source(Files.newInputStream(path, options));
-  }
+  ///** Returns a source that reads from {@code path}. */
+  //@IgnoreJRERequirement // Should only be invoked on Java 7+.
+  //public static Source source(Path path, OpenOption... options) throws IOException {
+  //  if (path == null) throw new IllegalArgumentException("path == null");
+  //  return source(Files.newInputStream(path, options));
+  //}
 
   /** Returns a sink that writes to {@code file}. */
   public static Sink sink(File file) throws FileNotFoundException {
@@ -187,12 +183,12 @@ public final class Okio {
     return sink(new FileOutputStream(file, true));
   }
 
-  /** Returns a sink that writes to {@code path}. */
-  @IgnoreJRERequirement // Should only be invoked on Java 7+.
-  public static Sink sink(Path path, OpenOption... options) throws IOException {
-    if (path == null) throw new IllegalArgumentException("path == null");
-    return sink(Files.newOutputStream(path, options));
-  }
+  ///** Returns a sink that writes to {@code path}. */
+  //@IgnoreJRERequirement // Should only be invoked on Java 7+.
+  //public static Sink sink(Path path, OpenOption... options) throws IOException {
+  //  if (path == null) throw new IllegalArgumentException("path == null");
+  //  return sink(Files.newOutputStream(path, options));
+  //}
 
   /** Returns a sink that writes nowhere. */
   public static Sink blackhole() {
